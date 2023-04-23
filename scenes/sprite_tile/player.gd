fb678d2e1e9e12978c40c03321137e1c2ab10ef1
ParadoxV5 <paradox.ver5@gmail.com>
2023-04-22 18:36:58 -0600

    主角移动和转身

--- scenes/sprite_tile/player.gd
+++ scenes/sprite_tile/player.gd
@@ -1,4 +1,4 @@
-extends "res://scenes/sprite_tile.gd"
+extends "../sprite_tile.gd"
 
 @export_group("Textures")
 var textures: Array[Texture2D] = [texture, null, null, null]
@@ -8,3 +8,23 @@ var textures: Array[Texture2D] = [texture, null, null, null]
   set(value): textures[2] = value
 @export var texture_up: Texture2D:
   set(value): textures[3] = value
+
+const TILE_SIZE: int = 32
+const VELOCITY_4DIR: PackedVector2Array = [
+  Vector2(0, TILE_SIZE),
+  Vector2(-TILE_SIZE, 0),
+  Vector2(TILE_SIZE, 0),
+  Vector2(0, -TILE_SIZE)
+]
+const INPUT_4DIR: PackedStringArray = [
+  &"ui_down", &"ui_left", &"ui_right", &"ui_up"
+]
+func _unhandled_input(event: InputEvent) -> void:
+  var facing2: int = 0
+  while(facing2 < 4):
+    if event.is_action_pressed(INPUT_4DIR[facing2]):
+      texture = textures[facing2]
+      var position2: Vector2 = position + VELOCITY_4DIR[facing2]
+      position = position2 # XXX: Future-proof
+      return
+    facing2 += 1
